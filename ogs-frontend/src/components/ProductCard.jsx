81f593fd9b127d6f9623e852f37c83802d487857
MiDev7 <razafintsalamamikaia@gmail.com>
2022-11-07 18:59:46 +0400

    Card structure

--- ogs-frontend/src/components/ProductCard.jsx
+++ ogs-frontend/src/components/ProductCard.jsx
@@ -1,36 +1,64 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import { Button, CardActionArea, CardActions, CardMedia, CardContent, Chip } from '@mui/material';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Rating from '@mui/material/Rating';
+import { makeStyles } from '@mui/styles';
+import axios from 'axios'
 
+const useStyles = makeStyles({
+  tagLine: {
+    fontWeight: '600',
+    color:'#413b3b',
+    fontSize: '8px'
+  },
+  card: {
+    padding: '0px',
+    margin:'0px'
+  }
+})
 
 function ProductCard() {
-  const [rating, setRating] = useState('0')
-  const handleChange = event => {
-    setRating(event.target.value)
-    console.log(event.target.value)
-  };
+  const classes = useStyles()
+  const [products, setProducts] = useState([])
+  // const [rating, setRating] = useState('0')
+  // const handleChange = event => {
+  //   setRating(event.target.value)
+  //   console.log(event.target.value)
+  // };
+  const url = 'https://dummyjson.com/products'
+  useEffect(()=>{
+    async function getProductData(){
+      const response =  await axios.get(url)
+      const data = response.data
+      console.log(data.products)
+      setProducts(data.products)
+    }
+
+    getProductData()
+  },[])
+
   return (
     <React.Fragment>
-      
-        <Card sx={{ maxWidth: 250, borderRadius: 3 }} variant='outlined'>
+      {
+        products.map((product)=>(
+          <Card key={product.id} sx={{ maxWidth: 260, borderRadius: 3 }} className={classes.card} variant='outlined'>
 
-          <CardActionArea>
+          <CardActionArea >
             <Chip icon={ <BookmarkIcon fontSize='small'/>}label='New' variant='tag' sx={{position:'absolute'}} color='primary'/>
             <CardMedia
             component="img"
             height="180"
-            image="https://images.unsplash.com/photo-1587925420649-89760ff7baab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1058&q=80"
+            image={product.images[0]}
             alt="green iguana"
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" fontWeight='600' sx={{color:'#413b3b'}} component="div">
-            Pack of 5 Chisels
-            </Typography>
-            <Typography gutterBottom variant='subtitle2' fontWeight='bold' color='primary'> In Stock</Typography>
+              <Typography variant="h5" fontWeight='600' height='75px' sx={{color:'#413b3b'}} component="div">
+              {product.title} <Typography component='span' color='rgba(66,115,31,1)' fontWeight='bold' variant='subtitle2'>In Stock</Typography>
+              </Typography> 
+            
             <Typography variant="body2" color="text.secondary" style={{height:'2.5rem', whiteSpace: 'pre-line',  overflow:'hidden',textOverflow: 'ellipsis'}}    >
             A pack of 5 high quality hand - made Chisels, extremely sharp and manufactured under High Quality Control. The Wood Handles are meticulously selected from hardwood timbers to ensure a comfortable yet secure grip while using. These handy tools can be used in slab entertaining and cutting curves in Plywood when building projects. 
 
@@ -43,18 +71,18 @@ function ProductCard() {
                 name="customized-10" 
                 color='#583f85' 
                 defaultValue={2} max={5} 
-                // onChange={handleChange} 
-                onChange={handleChange} value={parseFloat(rating)} 
+                // onChange={handleChange}  
+                value={parseFloat(product.rating)} 
                 precision={0.2}
                 sx={{
                   marginBlock:'10px',
                   padding:'0px',
                   marginInline:'0px'
-                }}/> {rating}.0
+                }}/> {product.rating.toFixed(1)}
             </Typography>
             
             <Typography variant="price" component='div'>
-              MUR 200.00
+              $ {product.price}
             </Typography>
             
           </CardContent>
@@ -66,7 +94,11 @@ function ProductCard() {
                 Add to Cart
               </Button>
           </CardActions>
-        </Card>
+          </Card>
+        ))
+      }
+      
+        
     </React.Fragment>
   )
 }
